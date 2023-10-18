9041fd006c03114e68a4d554a19711cf3515e760
VeraEnders <veraabramo@gmail.com>
2023-10-18 20:03:45 +0200

    add solution for homework 06-00

--- 06_retezce/du-06-00_uhodni-pohlavi-podle-prijmeni.py
+++ 06_retezce/du-06-00_uhodni-pohlavi-podle-prijmeni.py
@@ -1,13 +1,20 @@
 '''
 Ukol 0
 Vytvoř funkci, která dostane jako argument příjmení a zkusí podle něj uhodnout pohlaví. 
-Funkce bude vracet řetězec "žena" nebo "muž". Funkce bude součástí programu, 
+Funkce bude vracet řetězec 'žena' nebo 'muž'. Funkce bude součástí programu, 
 který se na příjmení zeptá a následně vypíše odhad pohlaví uživatele.
 '''
-def uhdni_pohlavi(prijmeni):
-    if prijmeni[-3:] == "ová":
-        return "žena"
-    else:
-        return "muž"
+def uhodni_pohlavi(prijmeni):
+  for znak in prijmeni:
+    if znak in '0123456789':
+      return 'mimozemšťan'
+  
+  if prijmeni[-1] == 'á':
+    return 'žena'
+  else:
+    return 'muž'
 
-prijmeni_uzivatele = input("Zadejte vase prijmeni")
+prijmeni_uzivatele = input('Zadejte Vaše příjmení: ')
+pohlavi_uzivatele = uhodni_pohlavi(prijmeni_uzivatele)
+
+print(f'Jste pravděpodobně {pohlavi_uzivatele}')
