5e4c7ac736ca704ae030e1bbe0e57a4d77a7f0ee
VeraEnders <veraabramo@gmail.com>
2023-10-23 02:09:33 +0200

    add solution of homework 07-03

--- 07_procvicovani-a-soubory/07-03_soucet-vetsi-nez-jeden-input.py
+++ 07_procvicovani-a-soubory/07-03_soucet-vetsi-nez-jeden-input.py
@@ -3,20 +3,17 @@ Ukol 3
 Napiš program, který se zeptá na 3 čísla a zjistí, jestli je jejich součet větší než 10. Dokážeš jej vymyslet tak, aby funkce input byla v kódu zapsaná jen jednou? ;)
 '''
 def je_soucet_vetsi():
-  pocet_cisel = 3
-  print(f'Zadejte postupně {pocet_cisel} čísel, pak vyhodnotím, jestli je jejich součet větší než 10.')
-
   soucet = 0
 
-  for i in range(pocet_cisel):
+  for i in range(3):
     while True:
       cislo = input('Zadejte číslo: ')
-      if not isinstance(cislo, int) or not isinstance(cislo, float):
+      if not cislo.isdigit():
         print('Špatný vstup. Nezadal/a jste číslo!')
       else:
-        soucet += float(cislo)
+        soucet += int(cislo)
         break
   
-  return soucet
+  return soucet > 10
 
-print(je_soucet_vetsi())
+print(f'Součet tří čísel je {"větší" if je_soucet_vetsi() else "menší"} než 10' )
