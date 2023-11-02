04b8e989d23cc6fd9e936287811d32bdb2347b5f
VeraEnders <veraabramo@gmail.com>
2023-11-02 21:34:55 +0100

    add solution for homework 08-05

--- 08_seznamy/du-08-05.py
+++ 08_seznamy/du-08-05.py
@@ -3,3 +3,12 @@ Ukol 5
 Napiš funkci, která dostane dvojici (tzv. tuple) a vrátí její součet. 
 Pokud funkce dostane více jak dvě čísla, tj. např. trojici, tak vypíše: "Bohužel, umím sečíst jen dvě čísla."
 '''
+
+def soucet_dvojice(tuple):
+  if len(tuple) != 2:
+    return 'Bohužel, umím sečíst jen dvě čísla.'
+  else:
+    return tuple[0] + tuple[1]
+  
+print(soucet_dvojice((1, 5)))
+print(soucet_dvojice((5, 2, 7)))
