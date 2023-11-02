979be1ac8e95248dc337c4e170bc1f7c3218e4c1
VeraEnders <veraabramo@gmail.com>
2023-11-02 19:32:46 +0100

    add solution for homework 08-00

--- 08_seznamy/du-08-00.py
+++ 08_seznamy/du-08-00.py
@@ -6,3 +6,10 @@ Domácí zvířata známe tato: "pes", "kočka", "králík", "had".
 Napiš funkci, která dostane jako argumenty seznam zvířat a slovo a zjistí, jestli je toto slovo v seznamu. 
 „Zjistí“ znamená, že funkce vrátí True nebo False.
 '''
+
+def je_slovo_v_seznamu(seznam, slovo):
+  return slovo in seznam
+
+zvirata = ["pes", "kočka", "králík", "had"]
+print(je_slovo_v_seznamu(zvirata, "had"))
+print(je_slovo_v_seznamu(zvirata, "krtek"))
