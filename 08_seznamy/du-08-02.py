3f8c1502f976a80f1bd603c2d89a4bf026f9639b
VeraEnders <veraabramo@gmail.com>
2023-11-02 20:03:14 +0100

    add solution for homework 08-02

--- 08_seznamy/du-08-02.py
+++ 08_seznamy/du-08-02.py
@@ -2,3 +2,13 @@
 Ukol 2
 Napiš funkci, která dostane jako argument seznam domácích zvířat a vrátí seznam těch, která začínají na k.
 '''
+
+def zacina_na_k(seznam):
+  vysledek = []
+  for slovo in seznam:
+    if slovo.lower().startswith('k'):
+      vysledek.append(slovo)
+  return vysledek
+
+domaci_zvirata = ['pes', 'kočka', 'křeček', 'králík', 'želva', 'činčila', 'had']
+print(zacina_na_k(domaci_zvirata))
