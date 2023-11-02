cad9164a5948ec02900d7866cd69a87da9bd3e80
VeraEnders <veraabramo@gmail.com>
2023-11-02 20:21:47 +0100

    add solution for homework 08-03

--- 08_seznamy/du-08-03.py
+++ 08_seznamy/du-08-03.py
@@ -2,3 +2,12 @@
 Ukol 3
 Napiš program, který seřadí seznam domácích zvířat podle abecedy. Víš, jaký je rozdíl mezi metodou sort a funkcí sorted?
 '''
+# Metoda sort() upravuje seznam na místě, nevrací nový seznam.
+# Funkce sorted() nemění vstup, vrací nový seřazený seznam.
+
+def serad_zvirata(seznam):
+  vysledek = sorted(seznam)
+  return vysledek
+
+domaci_zvirata = ['pes', 'kočka', 'křeček', 'králík', 'želva', 'činčila', 'had']
+print(serad_zvirata(domaci_zvirata))
