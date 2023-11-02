0654caffe39c7e4405149c7645ecbb24b236ba42
VeraEnders <veraabramo@gmail.com>
2023-11-02 19:54:28 +0100

    add solution for homework 08-01

--- 08_seznamy/du-08-01.py
+++ 08_seznamy/du-08-01.py
@@ -2,3 +2,12 @@
 Ukol 1
 Napiš funkci, která dostane jako argument seznam domácích zvířat a vrátí seznam těch, která jsou kratší než 5 písmen.
 '''
+def kratsi_nez_pet(seznam):
+  vysledek = []
+  for slovo in seznam:
+    if len(slovo) < 5:
+      vysledek.append(slovo)
+  return vysledek
+
+domaci_zvirata = ['pes', 'kočka', 'křeček', 'králík', 'želva', 'činčila', 'had']
+print(kratsi_nez_pet(domaci_zvirata))
