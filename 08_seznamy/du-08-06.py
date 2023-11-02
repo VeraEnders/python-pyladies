ca027d071a402891f53b78bdc014d0745f8dc662
VeraEnders <veraabramo@gmail.com>
2023-11-02 23:14:05 +0100

    add solution for homework 08-06

--- 08_seznamy/du-08-06.py
+++ 08_seznamy/du-08-06.py
@@ -1,9 +1,7 @@
 '''
 Ukol 6
 Had byl pyšný na to, že je v abecedě první. Dokud nepřiletěla "andulka".
-
 Abys hada uklidnila, vytvoř funkci, která zvířata seřadí podle abecedy, ale bude ignorovat první písmeno t.j. vrátí:
-
 "had",
 "pes",
 "andulka",
@@ -11,7 +9,6 @@ Abys hada uklidnila, vytvoř funkci, která zvířata seřadí podle abecedy, al
 "králík".
 
 Postup:
-
     Máš seznam hodnot, které chceš seřadit podle nějakého klíče. Klíč se dá z každé hodnoty vypočítat.
     Vytvoř seznam dvojic (klíč, hodnota).
     Seřaď tento seznam dvojic – dvojice se řadí nejdřív podle prvního prvku, pak druhého atd.
@@ -19,3 +16,18 @@ Postup:
 
 Proč má zrovna had takovéhle výsadní postavení, zjistíš později.
 '''
+def seradit(seznam):
+  seznam_dvojic = []
+  for zaznam in seznam:
+    seznam_dvojic.append((zaznam[1:], zaznam))
+  
+  serazene_dvojice = sorted(seznam_dvojic)
+
+  vysledek = []
+  for dvojice in serazene_dvojice:
+    vysledek.append(dvojice[1])
+  
+  return vysledek
+
+zvirata = ['králík', 'had', 'andulka', 'kočka', 'pes']
+print(seradit(zvirata))
