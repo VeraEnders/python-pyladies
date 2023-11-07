3c55c23030cf482359a40ed265148e302fe6e0b7
VeraEnders <veraabramo@gmail.com>
2023-11-07 18:54:11 +0100

    add tasks from lecture 09

---
+++ 09_slovniky/09_slovniky-vytvor-psika-kocicku.py
@@ -0,0 +1,78 @@
+import pytest
+#Ukol, uprav nasledujici funkce podle zadani nize
+
+#Funkce musi vratit slovnik, ktery:
+    # Obsahuje polozku s klicem 'jmeno' a hodnotou 'Chundelac'
+    # Obsahuje polozku s klicem 'rasa' a libovolnou hodnotou
+    # Obsahuje polozku s klicem 'vek' a libovolnou hodnotou
+def vytvor_psika():
+    pes = {
+        'jmeno': 'Chundelac',
+        'rasa': 'Poulicni smes',
+        'vek': 10
+    }
+    return pes
+
+#Uprav funkci tak, ze:
+    # Vaha kocky bude 11
+    # Rasa bude odstranena pomoci prikazu del
+    # pridej polozku 'vek' s hodnotou 5
+def vytvor_kocicku():
+    kocicka = {
+        'jmeno': 'Drobek',
+        'oblibene_krmivo': 'CatFood Premium Deluxe',
+        'vaha': 11,
+        'rasa': 'Poulicni smes',
+        'vek': 5
+    }
+
+    del kocicka['rasa']
+
+    return kocicka
+
+# print(vytvor_psika())
+# print(vytvor_kocicku())
+
+# Bonus: Zkontroluj spravnost sve implementace pomoci testu
+# 1. Ujistete se, ze mate vytvorene virtualni prostredi 'venv'
+#     - Instrukce zde: http://naucse.python.cz/2017/pyladies-praha-podzim-cznic/beginners/install/windows/
+#     - Prikaz k vytvoreni: py -3 -m venv venv
+#     - K aktivaci: \venv\Scripts\activate
+#     - Pokud je venv aktivni, na zacatku prikazove radky se ukaze (venv)
+#
+# 2. Instalujte knihovnu pytest
+#     - python -m pip install pytest
+#     - Pokud je vse v poradku: Successfully installed colorama-0.3.9 py-1.4.34 pytest-3.2.3
+#
+# 3. Spousteni testu
+#    - python -m pytest -v [jmeno-souboru.py]
+#    - python -m pytest -v 3-slovniky-ukol-zadani.py
+#    - uspesne spusteni: ========================== 1 passed in 0.03 seconds ===========================
+
+
+
+def test_psik_ma_spravne_jmeno():
+    pes = vytvor_psika()
+    assert pes['jmeno'] == 'Chundelac'
+
+def test_psik_ma_zadanou_rasu():
+    pes = vytvor_psika()
+    pes['rasa']
+
+def test_psik_ma_zadany_vek():
+    pes = vytvor_psika()
+    pes['vek']
+
+def test_kocicka_ma_zmenenou_vahu():
+    kocka = vytvor_kocicku()
+    assert kocka['vaha'] == 11
+
+def test_kocicka_ma_pridany_vek():
+    kocka = vytvor_kocicku()
+    assert kocka['vek'] == 5
+
+def test_kocicka_ma_odstranenu_rasu():
+    kocka = vytvor_kocicku()
+
+    with pytest.raises(KeyError):
+        kocka['rasa']
---
+++ 09_slovniky/09_vyhledavaci-tabulka.py
@@ -0,0 +1,7 @@
+zlate_stranky = {
+    'Alice': '654321978',
+    'Borivoj': '1475239980',
+    'Cyril': '654213978'
+}
+
+print(zlate_stranky['Borivoj'])
