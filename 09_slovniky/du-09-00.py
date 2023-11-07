433440f3129e5668f8c946376493fc1b49075d4a
VeraEnders <veraabramo@gmail.com>
2023-11-07 18:47:58 +0100

    add solution of homework 09-00

--- 09_slovniky/du-09-00.py
+++ 09_slovniky/du-09-00.py
@@ -7,3 +7,12 @@ Napiš funkci, která pro argumentem zadané číslo n vytvoří a vrátí slovn
 >>> mocniny(2)
 {1: 1, 2: 4}
 '''
+
+def mocniny(cislo):
+    vysledek = {}
+    for i in range(1, cislo + 1):
+        vysledek[i] = i ** 2
+    return vysledek
+
+print(mocniny(2))
+print(mocniny(4))
