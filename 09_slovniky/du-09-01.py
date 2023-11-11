36655fa7f6648ef258959183d0f77993aedfad25
VeraEnders <veraabramo@gmail.com>
2023-11-11 23:36:47 +0100

    add solution for homework 09-01

--- 09_slovniky/du-09-01.py
+++ 09_slovniky/du-09-01.py
@@ -8,3 +8,25 @@ Napiš funkci, která sečte a vrátí sumu všech klíčů a sumu všech hodnot
 >>> soucet_klicu_a_hodnot(muj_slovnik)
 (3, 15)
 '''
+def soucet_klicu_a_hodnot(slovnik):
+  # soucet_klicu = 0
+  # soucet_hodnot = 0
+
+  # for klic, hodnota in slovnik.items():
+  #   soucet_klicu += klic
+  #   soucet_hodnot += hodnota
+
+  soucet_klicu = sum(slovnik.keys())
+  soucet_hodnot = sum(slovnik.values())
+  return (soucet_klicu, soucet_hodnot)
+
+def mocniny(cislo):
+  vysledek = {}
+  for i in range(1, cislo + 1):
+    vysledek[i] = i ** 2
+  return vysledek
+
+print(soucet_klicu_a_hodnot(mocniny(4)))
+
+muj_slovnik = {0: 0, 1: 5, 2: 10}
+print(soucet_klicu_a_hodnot(muj_slovnik))
