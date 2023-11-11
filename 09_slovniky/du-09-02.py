206ca7d7bde9f217bb51db57c49f33dc0e4a86f9
VeraEnders <veraabramo@gmail.com>
2023-11-11 23:47:43 +0100

    add solution for homework 09-02

--- 09_slovniky/du-09-02.py
+++ 09_slovniky/du-09-02.py
@@ -8,3 +8,15 @@ Co vrátí funkce, když jejím argmentem bude "Máme rádi PyLadies!"?
 
 Zkus se obejít bez použítí metody count.
 '''
+def pocet_znaku(retezec):
+  slovnik = {}
+  for pismeno in retezec:
+    if slovnik.get(pismeno):
+      slovnik[pismeno] += 1
+    else:
+      slovnik[pismeno] = 1
+
+  return slovnik
+
+print(pocet_znaku("hello world"))
+print(pocet_znaku("Máme rádi PyLadies!"))
