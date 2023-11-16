6e23d1f356f690b46cbf81db6456729917f1488d
VeraEnders <veraabramo@gmail.com>
2023-11-16 17:39:24 +0100

    add solution of task du-10-00

--- 10_vyjimky-moduly/du-10-00.py
+++ 10_vyjimky-moduly/du-10-00.py
@@ -3,3 +3,17 @@ Ukol 0
 Napiš funkci, do které se vloží dvě čísla a ona vrátí jejich podíl. Pokud dojde na dělení nulou, tak vyhodí výjimku "Nulou dělit nelze!".
 '''
 
+def deleni():
+  while True:
+    try:
+      a = int(input('Zadejte cele cislo a: '))
+      b = int(input('Zadejte cele cislo b: '))
+
+      return a / b
+    
+    except ValueError:
+      print('Musíte zadat číslo! Zkuste znovu.')
+    except ZeroDivisionError:
+      print('Nulou dělit nelze!')
+
+print(deleni())
