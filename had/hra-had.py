eeb718f112c950f15ae4e3f91450735b5e2cd7bb
VeraEnders <veraabramo@gmail.com>
2023-11-20 00:19:13 +0100

    add new function pridej_ovoce

--- had/hra-had.py
+++ had/hra-had.py
@@ -1,6 +1,6 @@
-def nakresli_mapu(seznam_souradnic, rozmer):
-  "Nakreslí mřížku zadaného rozměru, na příslušná políčka doplní X, jinde tečku"
+import random
 
+def nakresli_mapu(seznam_souradnic, seznam_ovoce, rozmer):
   mapa = []
 
   for _ in range(rozmer):
@@ -9,12 +9,13 @@ def nakresli_mapu(seznam_souradnic, rozmer):
   for radek, pozice in seznam_souradnic:
     mapa[pozice][radek] = 'X'
 
+  for radek, pozice in seznam_ovoce:
+    mapa[pozice][radek] = '?'
+
   for i in mapa:
     print(' '.join(i))
 
-def pohyb(seznam_souradnic, svetova_strana, rozmer):
-  "Přidá k seznamu souřadnic poslední bod posunutý v určitém směru a smaže první bod"
-
+def pohyb(seznam_souradnic, seznam_ovoce, svetova_strana, rozmer):
   posledni_bod = seznam_souradnic[-1]
   x = posledni_bod[0]
   y = posledni_bod[1]
@@ -30,17 +31,34 @@ def pohyb(seznam_souradnic, svetova_strana, rozmer):
   
   novy_bod = (x, y)
 
-  if novy_bod in seznam_souradnic[:-1]:
+  if novy_bod in seznam_souradnic:
     raise ValueError('Game over.')
 
   if novy_bod[0] < 0 or novy_bod[1] < 0 or novy_bod[0] > (rozmer-1) or novy_bod[1] > (rozmer-1):
     raise ValueError('Game over.')
   
   seznam_souradnic.append(novy_bod)
-  del seznam_souradnic[0]
+  
+  if novy_bod in seznam_ovoce:
+    seznam_ovoce.remove(novy_bod)
+    if not seznam_ovoce:
+      pridej_ovoce(seznam_souradnic, seznam_ovoce, rozmer)
+  else:
+    del seznam_souradnic[0]
+
+def pridej_ovoce(seznam_souradnic, seznam_ovoce, rozmer):
+  while True:
+    x = random.randrange(rozmer)
+    y = random.randrange(rozmer)
+    nove_ovoce = (x, y)
+    if nove_ovoce not in seznam_ovoce and nove_ovoce not in seznam_souradnic:
+      seznam_ovoce.append(nove_ovoce)
+      break
 
 souradnice = [(0, 0), (1, 0), (2, 0)]
 rozmer_mapy = 10
+ovoce = []
+tah = 0
 
 while True:
   svetova_strana = input('Zadejte světovou stranu (s / j / z / v) nebo konec: ')
@@ -51,6 +69,11 @@ while True:
   if svetova_strana not in ['s', 'j', 'z', 'v']:
     print('Špatný vstup. Zkuste znovu.')
     continue
+
+  if tah % 30 == 0:
+    pridej_ovoce(souradnice, ovoce, rozmer_mapy)
   
-  pohyb(souradnice, svetova_strana, rozmer_mapy)
-  nakresli_mapu(souradnice, rozmer_mapy)
+  pohyb(souradnice, ovoce, svetova_strana, rozmer_mapy)
+  nakresli_mapu(souradnice, ovoce, rozmer_mapy)
+
+  tah += 1
